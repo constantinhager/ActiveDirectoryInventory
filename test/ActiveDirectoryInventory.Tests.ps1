a4c980eb202d87b7c18486499db25fff0205eb93
constantinhager <constantin.hager@outlook.de>
2017-10-28 23:56:46 +0200

    Start Writing Tests

--- test/ActiveDirectoryInventory.Tests.ps1
+++ test/ActiveDirectoryInventory.Tests.ps1
@@ -1,11 +1,16 @@
-[System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '', Scope='*', Target='SuppressImportModule')]
+[System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '', Scope = '*', Target = 'SuppressImportModule')]
 $SuppressImportModule = $true
 . $PSScriptRoot\Shared.ps1
 
-Describe 'Module Manifest Tests' {
+Describe 'Active Directory Module Tests' {
     It 'Passes Test-ModuleManifest' {
         Test-ModuleManifest -Path $ModuleManifestPath
         $? | Should Be $true
     }
+
+    It 'PowerShell Module for ActiveDirectory should be installed' {
+        Test-ADIActiveDirectoryModule | Should Be $true
+    }
 }
+
 
